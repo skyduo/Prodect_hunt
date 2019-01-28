fdbf441a6ad300c1a5c995ce99176d31b52e7ff6
skyduo <htd19941208@gmail.com>
2019-01-27 17:20:57 -0800

    Realize the login and logout function

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup_page'),
+    path('login/', views.login, name='login_page'),
+    path('logout/', views.logout, name='logout_page'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -19,3 +20,23 @@ def signup(request):
 				return redirect('mainpage')
 			else:
 				return render(request, 'signup.html', {'Wrong_password': 'Two passwords should be same'})
+
+
+def login(request):
+	if request.method == 'GET':
+		return render(request, 'login.html')
+	elif request.method == 'POST':
+		user_name = request.POST['User Name']
+		pass_word = request.POST['Password']
+		user = auth.authenticate(username=user_name, password=pass_word)
+		if user is None:
+			return render(request, 'login.html', {'Wrong': 'User name or password is wrong'})
+		else:
+			auth.login(request, user)
+			return redirect('mainpage')
+
+
+def logout(request):
+	if request.method == 'POST':
+		auth.logout(request)
+		return redirect('mainpage')
